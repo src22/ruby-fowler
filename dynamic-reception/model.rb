df5e221893083730a023fd193dae639938ac0da6
src22 <src22demo@gmail.com>
2016-09-13 12:12:16 +0200

    Model

---
+++ dynamic-reception/model.rb
@@ -0,0 +1,34 @@
+
+
+class Itinerary
+  def initialize
+    @items = []
+  end
+  def << arg
+    @items << arg
+  end
+  def items
+    return @items.dup
+  end
+end
+
+class Flight
+  attr_reader :airline, :from, :to
+  def initialize airline, from, to
+    @airline, @from, @to = airline, from, to
+  end
+  def to_s
+    return  "Flight: " + @airline + " " + @from + " " + @to + "  " 
+  end
+end
+
+class Hotel
+  attr_accessor :brand, :nights
+  def initialize brand, nights
+    @brand, @nights = brand, nights
+  end
+end
+
+# ----------------------------
+f = Flight.new "Aaa", "1aa", "2bb"
+puts "hello... " + f.to_s
---
+++ dynamic-reception/x.bat
@@ -0,0 +1,11 @@
+echo off
+cls
+echo .
+echo .
+echo .
+
+ruby model.rb
+
+echo .
+echo .
+echo .
